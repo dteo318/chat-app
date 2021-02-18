4347856436e2d4b24bd63cc8b634af6ae468e4cc
Daniel Teo <dteo318@gmail.com>
2021-02-18 18:02:46 +0800

    updated login screen using bootstrap

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# docker run -p 6379:6379 -d redis:5 
 def join_chat_view(request):
     return render(request, 'chat/join_chat.html')
 
