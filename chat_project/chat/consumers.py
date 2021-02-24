e20d8d323364a1389fa93a199604f2c09438788f
Daniel Teo <dteo318@gmail.com>
2021-02-24 18:53:16 +0800

    allowed room password to be updated as users in the room change it and as user joins room

--- chat_project/chat/consumers.py
+++ chat_project/chat/consumers.py
@@ -29,6 +29,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'messages': await sync_to_async(serializers.serialize)('json', room_messages)
         }))
 
+        password = await database_sync_to_async(self.get_password)(self.room_name)
+
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message_type': 'update_password',
+            'message': password
+        }))
+
     async def disconnect(self, close_code):
         await database_sync_to_async(self.disconnect_user)(self.username, self.room_name)
 
@@ -47,41 +55,65 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        current_user = text_data_json['username']
-        message = text_data_json['message']
         message_type = text_data_json['message_type']
 
-        if message_type == 'save_message':
-            await database_sync_to_async(self.save_message)(current_user, self.room_name, message)
-            print("CREATE SAVED MESSAGE")
-            
-        elif message_type == 'unsave_message':
-            await database_sync_to_async(self.unsave_message)(current_user, self.room_name, message)
-            print("DELETE SAVED MESSAGE")
-        else:
-            # Send message to room group
+        if message_type == 'update_password':
+            password = text_data_json['password']
+            await database_sync_to_async(self.update_room_password)(self.room_name, password)
+            print("PASSWORD UPDATED")
+            # Send new password to all channels
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'chat_message',
-                    'message_type': message_type,
-                    'username' : current_user,
-                    'message': message
+                    'message_type': "update_password",
+                    'message': password
                 }
             )
 
+        else:
+            current_user = text_data_json['username']
+            message = text_data_json['message']
+
+            if message_type == 'save_message':
+                await database_sync_to_async(self.save_message)(current_user, self.room_name, message)
+                print("CREATE SAVED MESSAGE")
+                
+            elif message_type == 'unsave_message':
+                await database_sync_to_async(self.unsave_message)(current_user, self.room_name, message)
+                print("DELETE SAVED MESSAGE")
+                
+            else:
+                # Send message to room group
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'chat_message',
+                        'message_type': message_type,
+                        'username' : current_user,
+                        'message': message
+                    }
+                )
+
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
-        current_user = event['username']
+        # Send message to WebSocket
         message_type = event['message_type']
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-                'message_type': message_type,
-                'message': message,
-                'username' : current_user
-            }))
+        if message_type == 'update_password':
+            message = event['message']
+            await self.send(text_data=json.dumps({
+                    'message_type': message_type,
+                    'message': message,
+                }))
+        else:
+            message = event['message']
+            current_user = event['username']
+            await self.send(text_data=json.dumps({
+                    'message_type': message_type,
+                    'message': message,
+                    'username' : current_user
+                }))
 
     def disconnect_user(self, user_name, room_name):
         room_model = Room.objects.get(group_name=room_name)
@@ -123,7 +155,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def get_room_messages(self, room_name):
         room_model = Room.objects.get(group_name=room_name)
         room_messages = room_model.room_message.all().order_by("-sent_date")
-        print(room_messages)
         return room_messages
 
     def no_more_connections(self, room_name):
@@ -132,3 +163,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def delete_room(self, room_name):
         room_model = Room.objects.get(group_name=room_name)
         room_model.delete()
+
+    def update_room_password(self, room_name, password):
+        room_model = Room.objects.get(group_name=room_name)
+        room_model.password = password
+        room_model.save(update_fields=['password'])
+
+    def get_password(self, room_name):
+        room_model = Room.objects.get(group_name=room_name)
+        return room_model.password
